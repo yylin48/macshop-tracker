d95cc8ff43c9ca0166fc02ff042961449d8a84ad
yylin48 <44919194+yylin48@users.noreply.github.com>
2019-03-02 20:09:27 +0800

    v2.0

--- app.py
+++ app.py
@@ -1,114 +1,107 @@
+import time, requests, datetime
+from datetime import datetime , timedelta
 from bs4 import BeautifulSoup
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-import requests ,time, datetime
-from datetime import datetime ,timedelta
 from apscheduler.schedulers.blocking import BlockingScheduler
-
-
-###secret data
-line_bot_api = LineBotApi('channel token')
-handler = WebhookHandler('secret')
-your_id = 'your id'
-
-#data can be modified
-content = "iphone"
-url = 'https://www.ptt.cc/bbs/macshop/index.html'
-
-###scheduler
+from linebot import LineBotApi, WebhookHandler
+from env import *
+from linebot.models import TextSendMessage
+#scheduler
 sched = BlockingScheduler()
-#time unit
-onehour = timedelta(hours=1)
-oneday = timedelta(days=1)
 
-def getsoup(url):
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text , 'html.parser')
-    return soup
+###channel token
+line_bot_api = LineBotApi(channel_token)
+###secret
+handler = WebhookHandler(secret)
 
-def next_page(url):
-    soup = getsoup(url)
-    btns = soup.find_all("a", class_="btn wide")
-    for btn in btns:
-        btn_name = btn.text
-        if '‹ 上頁' in btn_name:
-            btn_url = 'https://www.ptt.cc' + btn.get('href')
-            return btn_url
 
-#爬蟲function
-def find_key(url, keyword, curtime):
-    #初始
-    dict_ptt = {}
-    count = 0
-    top = 0
-    #調整時間格式
-    curdate = ' %d/%d' % (curtime.month , curtime.day)
-    yesterdate = ' %d/%d' % ((curtime - oneday).month , (curtime - oneday).day)
-    #爬最新頁
-    soup = getsoup(url)
-    result = soup.find_all("div", class_="r-ent")
-    for article in result:
-        a_article = article.select_one("a")
-        title_url = 'https://www.ptt.cc' + a_article.get('href')
-        title_name = article.find("div", class_="title").text
-        title_date = article.find("div" , class_ = "date").text
-        #日期判斷(今天加昨天的日期標籤都當作有效的，會抓時間標籤做一小時內的判斷)
-        if title_date == curdate or title_date == yesterdate:
-            soup2 = getsoup(title_url)
-            metaline = soup2.find_all("div" , class_ = "article-metaline")
-            for meta in metaline:
-                metatext = meta.find("span", class_ = "article-meta-tag").text
-                #進下一層抓時間
-                if '時間' in metatext:
-                    #title_time_text's form : "Fri Feb 22 16:37:53 2019"
-                    title_time_text = meta.find("span" , class_ = "article-meta-value").text
-                    #title_time's form : "2019-02-22 16:37:53"
-                    title_time = datetime.strptime(title_time_text , '%a %b %d %H:%M:%S %Y')
-                    #判斷時間
-                    if title_time + onehour > curtime:
-                        #count為非置頂且一小時內的項目
-                        count += 1
-                        # 判斷關鍵字
-                        if keyword.upper() in title_name.upper() and '販售' in title_name:
-                            dict_ptt.update({title_name : title_url})
-        #置頂文章判斷
-        else:
-            top += 1
-    #置頂＋需要爬第二頁
-    if len(result) <= count + top and top > 0:
-        dict2 = find_key(next_page(url) , keyword ,curtime)
-        dict_ptt.update(dict2)
-    return dict_ptt
+#time
+onehour = timedelta(hours = 1)
+oneday = timedelta(days = 1)
+eighthour = timedelta(hours= 8)
+macshop_url = 'https://www.ptt.cc/bbs/macshop/index.html'
 
-def push_messager(data , user_id):
-    line_bot_api.push_message(user_id, TextSendMessage(text=data))
-    return True
+class crawler():
+    def __init__(self , keyword ,url):
+        self.keyword = keyword
+        self.url = url
+        self.user_id = []
+        self.list = ""
+        self.count = 0
 
-def make_token(content, datasize, answer):
-    dataform = '一小時內%s的貼文數:' % content + '%d' % datasize
-    goodtoken = "{}\n{}".format(dataform, answer)
-    return goodtoken
+    def getsoup(self):
+        res = requests.get(self.url)
+        return BeautifulSoup(res.text, 'html.parser')
 
-def dic_to_list(dict):
-    list = ""
-    for i in dict.items():
-        list += i[0]
-        list += i[1]
-    return list
+    def next_page(self):
+        btns = self.getsoup().find_all("a", class_="btn wide")
+        for btn in btns:
+            btn_name = btn.text
+            if '‹ 上頁' in btn_name:
+                btn_url = 'https://www.ptt.cc' + btn.get('href')
+                self.url = btn_url
+
+    def add_user_id(self , new_user_id):
+        self.user_id.append(new_user_id)
+
+    def time_update(self):
+        self.curtime = datetime.fromtimestamp(time.time()) + eighthour
+        self.yestertime = self.curtime - oneday
+        self.curdate = ' %d/' % self.curtime.month + self.curtime.strftime("%d")
+        self.yesterdate = ' %d/' % self.yestertime.month + self.yestertime.strftime("%d")
+
+    def find_key(self):
+        dict_ptt = {}
+        count = 0
+        top = 0
+        result = self.getsoup().find_all("div", class_="r-ent")
+        for article in result:
+            a_article = article.select_one("a")
+            title_url = 'https://www.ptt.cc' + a_article.get('href')
+            title_name = article.find("div", class_="title").text
+            title_date = article.find("div", class_="date").text
+            # 日期判斷(今天加昨天的日期標籤都當作有效的，反正之後會抓時間標籤做一小時內的判斷)
+            if title_date == self.curdate or title_date == self.yesterdate:
+                res2 = requests.get(title_url)
+                soup2 = BeautifulSoup(res2.text, 'html.parser')
+                metaline = soup2.find_all("div", class_="article-metaline")
+                for meta in metaline:
+                    metatext = meta.find("span", class_="article-meta-tag").text
+                    # 進下一層抓時間
+                    if '時間' in metatext:
+                        # title_time_text's form : "Fri Feb 22 16:37:53 2019"
+                        title_time_text = meta.find("span", class_="article-meta-value").text
+                        # title_time's form : "2019-02-22 16:37:53"
+                        title_time = datetime.strptime(title_time_text, '%a %b %d %H:%M:%S %Y')
+                        # 判斷時間
+                        if title_time + onehour > self.curtime:
+                            count += 1
+                            # 判斷關鍵字
+                            if self.keyword.upper() in title_name.upper() and '販售' in title_name:
+                                dict_ptt.update({title_name: title_url})
+            # 置頂文章判斷
+            else:
+                top += 1
+        if len(result) <= count + top and top > 0:
+            self.next_page()
+            self.find_key()
+        for i in dict_ptt.items():
+            self.list += i[0]
+            self.list += i[1]
+            self.count += 1
+
+    def token(self):
+        dataform = '一小時內%s的貼文數:' % self.keyword + '%d' % self.count
+        goodtoken = "{}{}".format(dataform, self.list)
+        return goodtoken
 
 @sched.scheduled_job('cron', hour='0-23' , minute=0)
 def myjob():
-    curtime = datetime.fromtimestamp(time.time()) + timedelta(hours=8)
-    reply_dict = find_key(url, content, curtime)
-    answer = dic_to_list(reply_dict)
-    mytoken = make_token(content, len(reply_dict), answer)
-    push_messager(mytoken , daisy_id)
-    push_messager(mytoken, your_id)
+    a = crawler("iphone", macshop_url)
+    a.time_update()
+    a.find_key()
+    a.add_user_id(your_id)
+    a.add_user_id(daisy_id)
+    for id in a.user_id:
+        line_bot_api.push_message(id, TextSendMessage(text=a.token()))
 
 sched.start()
-
