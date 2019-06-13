8a2debc62d61ed166587437cf87b44f1c1dbe7c2
Yilin Lee <asd840408@gmail.com>
2019-06-13 22:51:40 +0800

    add login api

--- flask/app.py
+++ flask/app.py
@@ -1,18 +1,62 @@
 # -*- coding:utf8 -*-
 import os, json, time, datetime, logging
 from pymongo import MongoClient
-from flask import (Flask, make_response, render_template, jsonify)
+from flask import (Flask, make_response, render_template, jsonify, request)
 from flask_cors import CORS
+from flask_jwt_extended import (JWTManager, jwt_required, create_access_token, get_jwt_identity)
 
+#setup 
 app = Flask(__name__)
 CORS(app)
-
+jwt = JWTManager(app)
+app.config['JWT_SECRET_KEY'] =  'Oa3La13adDLOsFPdg8924098713h98hfoiew'
+#DB connect
 def connect_mongo():
     client = MongoClient('database',27017)
     db = client['test']
     return db
 
-@app.route('/show')
+########
+# apis #
+########
+
+@app.route('/', methods=['GET'])
+def hello():
+    return "Hello World!"
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    r = make_response(jsonify(health='Well done.'), 200)
+    r.add_etag()
+    return r
+
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    if username != 'admin' or password != 'daisy':
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    expires = datetime.timedelta(days = 1)
+    # Identity can be any data that is json serializable
+    access_token = create_access_token(identity=username,expires_delta=expires)
+    return jsonify(access_token=access_token), 200
+
+@app.route('/protected', methods=['GET'])
+@jwt_required
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+@app.route('/show', methods=['GET'])
 def show():
     db = connect_mongo()
     collection = 'user_list'
